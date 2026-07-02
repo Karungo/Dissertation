8413119e74c9dfd0a1af8744fca78d10998439e6
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-02 22:37:46 +0100

    Update model_loader.py

--- app/services/model_loader.py
+++ app/services/model_loader.py
@@ -1,6 +1,6 @@
 import os
 import tensorflow as tf
-from config import MODEL_PATH
+from config import MODEL_BLOB
 
 model = None
 
@@ -13,13 +13,13 @@ def load_model():
         return model
 
     # Ensure model exists locally
-    if not os.path.exists(MODEL_PATH):
+    if not os.path.exists(MODEL_BLOB):
         raise FileNotFoundError(
-            f"Model file not found at {MODEL_PATH}. "
+            f"Model file not found at {MODEL_BLOB}. "
             "Please ensure the model is downloaded or included in the repo."
         )
 
     # Load model once
-    model = tf.keras.models.load_model(MODEL_PATH)
+    model = tf.keras.models.load_model(MODEL_BLOB)
 
     return model
