1ff2f409ba3036dc21c9a7b285a5ee84615ae529
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 22:42:44 +0100

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,13 +1,8 @@
 from fastapi import APIRouter, UploadFile, File, Form
-
 from services.pipeline import analyze_image
 
-router = APIRouter(tags=["Wildlife"])
-
+router = APIRouter()
 
 @router.post("/analyze")
-async def analyze(
-    image: UploadFile = File(...),
-    question: str = Form(...)
-):
+async def analyze(image: UploadFile = File(...), question: str = Form(...)):
     return await analyze_image(image, question)
