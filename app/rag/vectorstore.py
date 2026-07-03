543814687f6bf7ad177e7e1cb725b6a99f2a3de9
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 21:34:51 +0100

    Update vectorstore.py

--- app/rag/vectorstore.py
+++ app/rag/vectorstore.py
@@ -1,22 +1,41 @@
-import threading
-
+import os
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from ml.loader import download_blob
+from core.config import (
+    BUCKET_NAME,
+    FAISS_INDEX_BLOB,
+    FAISS_META_BLOB
+)
 
-vectorstore = None
+VECTOR_DIR = "/tmp/vectorstore"
+
+_vectorstore = None
 
 
 def load_vectorstore():
-    global vectorstore
+    global _vectorstore
 
-    if vectorstore is not None:
-        return vectorstore
+    if _vectorstore:
+        return _vectorstore
 
-    ...
-    vectorstore = FAISS.load_local(
-        VECTOR_DIR,
-        embeddings,
-        allow_dangerous_deserialization=True,
+    os.makedirs(VECTOR_DIR, exist_ok=True)
+
+    index_path = f"{VECTOR_DIR}/index.faiss"
+    meta_path = f"{VECTOR_DIR}/index.pkl"
+
+    if not os.path.exists(index_path):
+        download_blob(BUCKET_NAME, FAISS_INDEX_BLOB, index_path)
+        download_blob(BUCKET_NAME, FAISS_META_BLOB, meta_path)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
     )
 
-    return vectorstore
+    _vectorstore = FAISS.load_local(
+        VECTOR_DIR,
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    return _vectorstore
