6d1cf1a2cdaae2f5c6b99f4fe5394f6d3fdfa1fc
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 20:25:15 +0100

    Update gemini.py

--- app/llm/gemini.py
+++ app/llm/gemini.py
@@ -1,18 +1,17 @@
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    google_api_key=os.environ["GOOGLE_API_KEY"]
+    model="gemini-2.5-flash"
 )
+
 
 def generate_answer(question, predictions, docs):
 
-    context = "\n\n".join(d.page_content for d in docs)
+    context = "\n\n".join(
+        d.page_content for d in docs
+    )
 
     prompt = f"""
-You are a wildlife expert.
-
 Predictions:
 {predictions}
 
@@ -22,7 +21,7 @@ Context:
 Question:
 {question}
 
-Answer using only context.
+Answer using only the context.
 """
 
     return llm.invoke(prompt).content
