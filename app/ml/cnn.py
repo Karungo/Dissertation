7f061bbe853e60f60f49c3248186cf9d6911c684
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 20:21:11 +0100

    Update cnn.py

--- app/ml/cnn.py
+++ app/ml/cnn.py
@@ -1,31 +1,36 @@
-import numpy as np
 import json
+import numpy as np
 import tensorflow as tf
+
 from PIL import Image
 
-from core.config import CLASS_NAMES_PATH
-from ml.loader import get_model
-
-with open(CLASS_NAMES_PATH, "r") as f:
-    CLASS_NAMES = json.load(f)
+from ml.loader import load_model
 
 IMG_SIZE = 224
 
+with open("models/class_names.json") as f:
+    CLASS_NAMES = json.load(f)
+
 
 def preprocess(image):
+
     image = image.resize((IMG_SIZE, IMG_SIZE))
+
     arr = np.array(image)
 
     arr = tf.keras.applications.efficientnet.preprocess_input(arr)
 
-    return np.expand_dims(arr, axis=0)
+    return np.expand_dims(arr, 0)
 
 
 def predict_species(image, top_k=3):
-    model = get_model()
-    arr = preprocess(image)
 
-    preds = model.predict(arr, verbose=0)[0]
+    model = load_model()
+
+    preds = model.predict(
+        preprocess(image),
+        verbose=0
+    )[0]
 
     indices = np.argsort(preds)[::-1][:top_k]
 
