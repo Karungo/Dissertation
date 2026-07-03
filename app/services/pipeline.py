6924f94ccff302556511301145ceae309204e80f
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 19:42:21 +0100

    Update pipeline.py

--- app/services/pipeline.py
+++ app/services/pipeline.py
@@ -1,58 +1,22 @@
 from PIL import Image
+from ml.cnn import predict_species
+from rag.retrieval import retrieve_context
+from llm.gemini import generate_answer
 
-from services.cnn_service import (
-    predict_species
-)
 
-from services.retrieval_service import (
-    retrieve_context
-)
+async def analyze_image(upload_file, question):
 
-from services.gemini_service import (
-    generate_answer
-)
+    image = Image.open(upload_file.file)
 
-async def analyze_image(
-    upload_file,
-    question
-):
-
-    image = Image.open(
-        upload_file.file
-    )
-
-    predictions = (
-        predict_species(
-            image,
-            top_k=3
-        )
-    )
+    predictions = predict_species(image, top_k=3)
 
     docs = []
+    for p in predictions:
+        docs.extend(retrieve_context(p["species"], question))
 
-    for prediction in predictions:
-
-        species = prediction[
-            "species"
-        ]
-
-        docs.extend(
-            retrieve_context(
-                species,
-                question
-            )
-        )
-
-    answer = generate_answer(
-        question,
-        predictions,
-        docs
-    )
+    answer = generate_answer(question, predictions, docs)
 
     return {
-        "predictions":
-            predictions,
-
-        "answer":
-            answer
+        "predictions": predictions,
+        "answer": answer
     }
