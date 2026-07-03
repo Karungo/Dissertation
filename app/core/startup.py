de5694cd86445d5a00ee95212d2f49d381aee4a0
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 22:39:29 +0100

    Update startup.py

--- app/core/startup.py
+++ app/core/startup.py
@@ -1,28 +1,7 @@
-from contextlib import asynccontextmanager
-import asyncio
-
 from ml.loader import load_model
 from rag.vectorstore import load_vectorstore
 
-
-@asynccontextmanager
-async def lifespan(app):
-
-    loop = asyncio.get_running_loop()
-
-    model_task = loop.run_in_executor(
-        None,
-        load_model
-    )
-
-    vector_task = loop.run_in_executor(
-        None,
-        load_vectorstore
-    )
-
-    await asyncio.gather(
-        model_task,
-        vector_task
-    )
-
-    yield
+def startup():
+    # preload heavy stuff ONCE
+    load_model()
+    load_vectorstore()
