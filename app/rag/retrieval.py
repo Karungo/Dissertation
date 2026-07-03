6157e841cc22bab02c0ec45c5dac6d462a029505
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 22:44:40 +0100

    Update retrieval.py

--- app/rag/retrieval.py
+++ app/rag/retrieval.py
@@ -1,11 +1,9 @@
 from rag.vectorstore import load_vectorstore
 
-
 def retrieve_context(species, question):
+    vs = load_vectorstore()
 
-    vectorstore = load_vectorstore()
-
-    return vectorstore.similarity_search(
-        f"{species} {question}",
-        k=3
+    return vs.similarity_search(
+        query=f"{species} {question}",
+        k=5
     )
