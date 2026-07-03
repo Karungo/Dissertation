62e0742108fdd3e414e1c95a864725b9e71db449
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 21:17:21 +0100

    Update vectorstore.py

--- app/rag/vectorstore.py
+++ app/rag/vectorstore.py
@@ -3,28 +3,20 @@ import threading
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
-_vectorstore = None
-_lock = threading.Lock()
+vectorstore = None
 
 
 def load_vectorstore():
+    global vectorstore
 
-    global _vectorstore
+    if vectorstore is not None:
+        return vectorstore
 
-    if _vectorstore is None:
+    ...
+    vectorstore = FAISS.load_local(
+        VECTOR_DIR,
+        embeddings,
+        allow_dangerous_deserialization=True,
+    )
 
-        with _lock:
-
-            if _vectorstore is None:
-
-                embeddings = HuggingFaceEmbeddings(
-                    model_name="sentence-transformers/all-mpnet-base-v2"
-                )
-
-                _vectorstore = FAISS.load_local(
-                    "/tmp/vectorstore",
-                    embeddings,
-                    allow_dangerous_deserialization=True
-                )
-
-    return _vectorstore
+    return vectorstore
