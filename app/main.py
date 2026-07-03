965e2953e2afc7495e6b4a38cc42ba9f42ae48a7
Alvin Karungo <66041284+Karungo@users.noreply.github.com>
2026-07-03 20:45:26 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from api.routes import router
-from core.startup import startup
+from core.startup import lifespan
 
-app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
-    startup()
+app = FastAPI(
+    title="Wildlife Identification API",
+    version="1.0",
+    lifespan=lifespan
+)
 
 app.include_router(router)
 
+
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {"status": "healthy"}
